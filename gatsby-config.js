fd8e945ab52d8e315c4e170fabdf077f0ed929c0
Funny Rose <fannyperretlombard@gmail.com>
2020-11-08 16:29:42 +0100

    Added gatsby mdx plugin and sharp library :tada:

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,5 +22,21 @@ module.exports = {
         path: `${___dirname}/src/images`,
       },
     },
+    `gatsby-plugin-sharp`, 
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    }, 
   ],
 }
