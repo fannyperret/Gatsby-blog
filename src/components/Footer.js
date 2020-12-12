48c5e479a8cd3b727563e0c43a27ac129d3e5a72
Funny Rose <fannyperretlombard@gmail.com>
2020-12-12 02:25:44 +0100

    Added footer :cat:

---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { 
+    FooterWrapper, 
+    FooterSocialWrapper, 
+    FooterSocialIcons 
+} from "../elements"
+
+export const Footer = () => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            github: file(relativePath: {eq: "github.svg"}) {
+                publicURL
+            }
+            github: file(relativePath: {eq: "github.svg"}) {
+                publicURL
+            }
+            instagram: file(relativePath: {eq: "instagram.svg"}) {
+                publicURL
+            }
+            twitter: file(relativePath: {eq: "twitter.svg"}) {
+                publicURL
+            }
+        }
+    `)
+    return (
+        <FooterWrapper>  
+            <FooterSocialWrapper>
+                <FooterSocialIcons>
+                    <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                        <img src={data.github.publicURL} alt="Github logo" />
+                    </a>
+                    <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                        <img src={data.github.publicURL} alt="Github logo" />
+                    </a>
+                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                        <img src={data.instagram.publicURL} alt="Instagram logo" />
+                    </a>
+                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                        <img src={data.twitter.publicURL} alt="Twitter logo" />
+                    </a>
+                </FooterSocialIcons>
+                <p>© 2021 DansTonDev. Tous droits réservés.</p>
+            </FooterSocialWrapper>
+        </FooterWrapper>
+    )
+}
