a577e40e9699ec44509aa28b4513269ca181de05
Funny Rose <fannyperretlombard@gmail.com>
2020-12-13 03:10:52 +0100

    Added feature image :cat:

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
-import { Container } from "../components"
+import { Container, FeatureImage } from "../components"
 
 const IndexPage = () => {
   return (
-    <Container></Container>
+    <Container>
+      <FeatureImage />
+    </Container>
   )
 }
 
-export default IndexPage;
+export default IndexPage;  
